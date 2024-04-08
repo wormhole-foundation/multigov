64d4aef0c7d82a53f631fd9cd0a4e5c9141389af
keating <keating.dev@protonmail.com>
2024-04-08 08:43:08 -0400

    Fix formatting

--- src/HubProposalMetadata.sol
+++ src/HubProposalMetadata.sol
@@ -4,15 +4,15 @@ pragma solidity ^0.8.23;
 import {IGovernor} from "@openzeppelin-contracts/governance/IGovernor.sol";
 
 contract HubProposalMetadata {
-  IGovernor immutable governor;
+  IGovernor immutable GOVERNOR;
 
   constructor(address _governor) {
-    governor = IGovernor(_governor);
+    GOVERNOR = IGovernor(_governor);
   }
 
   function getProposalMetadata(uint256 _proposalId) external view returns (uint256, uint256, uint256) {
-    uint256 voteStart = governor.proposalSnapshot(_proposalId);
-    uint256 voteEnd = governor.proposalDeadline(_proposalId);
+    uint256 voteStart = GOVERNOR.proposalSnapshot(_proposalId);
+    uint256 voteEnd = GOVERNOR.proposalDeadline(_proposalId);
     return (_proposalId, voteStart, voteEnd);
   }
 }
