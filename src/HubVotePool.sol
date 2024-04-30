557b54da2741f5a99a0ebd4f4e3364aa5c8468ca
Alexander Keating <keating.dev@protonmail.com>
2024-04-30 14:41:35 -0400

    Change Hub vote aggregator to queries (#36)
     Co-authored-by: wildmolasses <changes@gmail.com>

--- src/HubVotePool.sol
+++ src/HubVotePool.sol
@@ -4,8 +4,15 @@ pragma solidity ^0.8.23;
 import {IWormhole} from "wormhole/interfaces/IWormhole.sol";
 import {IGovernor} from "@openzeppelin-contracts/governance/IGovernor.sol";
 import {ERC20Votes} from "@openzeppelin-contracts/token/ERC20/extensions/ERC20Votes.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {
+  QueryResponse,
+  ParsedQueryResponse,
+  ParsedPerChainQueryResponse,
+  EthCallQueryResponse
+} from "wormhole/query/QueryResponse.sol";
 
-contract HubVotePool {
+contract HubVotePool is QueryResponse, Ownable {
   IWormhole public immutable WORMHOLE_CORE;
   IGovernor public immutable HUB_GOVERNOR;
   uint8 constant UNUSED_SUPPORT_PARAM = 1;
@@ -13,10 +20,14 @@ contract HubVotePool {
   error InvalidWormholeMessage(string);
   error UnknownMessageEmitter();
   error InvalidProposalVote();
+  error TooManyEthCallResults(uint256);
+  error TooManyQueryResponses(uint256);
 
   event SpokeVoteCast(
     uint16 indexed emitterChainId, uint256 proposalId, uint256 voteAgainst, uint256 voteFor, uint256 voteAbstain
   );
+
+  event SpokeRegistered(uint16 indexed targetChain, bytes32 spokeVoteAddress);
 
   /// @dev Contains the distribution of a proposal vote.
   struct ProposalVote {
@@ -25,38 +36,66 @@ contract HubVotePool {
     uint128 abstainVotes;
   }
 
+  struct SpokeVoteAggregator {
+    uint16 wormholeChainId;
+    address addr;
+  }
+
   mapping(uint16 emitterChain => bytes32 emitterAddress) public spokeRegistry;
 
   // Instead of nested mapping create encoding for the key
   mapping(bytes32 spokeProposalId => ProposalVote proposalVotes) public spokeProposalVotes;
 
-  constructor(address _core, address _hubGovernor) {
+  constructor(address _core, address _hubGovernor, SpokeVoteAggregator[] memory _initialSpokeRegistry)
+    QueryResponse(_core)
+    Ownable()
+  {
     WORMHOLE_CORE = IWormhole(_core);
     HUB_GOVERNOR = IGovernor(_hubGovernor);
-    // TODO: delegate
-    // ERC20Votes(IFractionalGovernor(address(HUB_GOVERNOR)).token()).delegate(address(this));
+    transferOwnership(_hubGovernor);
+    for (uint256 i = 0; i < _initialSpokeRegistry.length; i++) {
+      SpokeVoteAggregator memory aggregator = _initialSpokeRegistry[i];
+      spokeRegistry[aggregator.wormholeChainId] = bytes32(uint256(uint160(aggregator.addr)));
+      emit SpokeRegistered(aggregator.wormholeChainId, bytes32(uint256(uint160(aggregator.addr))));
+    }
   }
 
-  function receiveMessage(bytes memory _encodedMessage) public {
-    // call the Wormhole core contract to parse and verify the encodedMessage
-    (IWormhole.VM memory wormholeMessage, bool valid, string memory reason) =
-      WORMHOLE_CORE.parseAndVerifyVM(_encodedMessage);
+  function registerSpoke(uint16 _targetChain, bytes32 _spokeVoteAddress) external {
+    _checkOwner();
+    spokeRegistry[_targetChain] = _spokeVoteAddress;
+    emit SpokeRegistered(_targetChain, _spokeVoteAddress);
+  }
 
-    if (!valid) revert InvalidWormholeMessage(reason);
-    if (wormholeMessage.emitterAddress != spokeRegistry[wormholeMessage.emitterChainId]) revert UnknownMessageEmitter();
+  // TODO we will need a Solana method as well
+  function crossChainEVMVote(bytes memory _queryResponseRaw, IWormhole.Signature[] memory _signatures) external {
+    // Validate the query response signatures
+    ParsedQueryResponse memory _queryResponse = parseAndVerifyQueryResponse(_queryResponseRaw, _signatures);
+
+    uint256 numResponses = _queryResponse.responses.length;
+    if (numResponses != 1) revert TooManyQueryResponses(numResponses);
+
+    // Validate that the query response is from hub
+    ParsedPerChainQueryResponse memory perChainResp = _queryResponse.responses[0];
+
+    EthCallQueryResponse memory _ethCalls = parseEthCallQueryResponse(perChainResp);
+
+    // verify contract and chain is correct
+    bytes32 addr = spokeRegistry[perChainResp.chainId];
+    if (addr != bytes32(uint256(uint160(_ethCalls.result[0].contractAddress)))) revert UnknownMessageEmitter();
+
+    if (_ethCalls.result.length != 1) revert TooManyEthCallResults(_ethCalls.result.length);
 
     (uint256 proposalId, uint128 againstVotes, uint128 forVotes, uint128 abstainVotes) =
-      abi.decode(wormholeMessage.payload, (uint256, uint128, uint128, uint128));
+      abi.decode(_ethCalls.result[0].result, (uint256, uint128, uint128, uint128));
 
     // TODO: does encode vs encodePacked matter here
-    bytes32 _spokeProposalId = keccak256(abi.encode(wormholeMessage.emitterChainId, proposalId));
+    bytes32 _spokeProposalId = keccak256(abi.encode(perChainResp.chainId, proposalId));
     ProposalVote memory existingSpokeVote = spokeProposalVotes[_spokeProposalId];
     if (
       existingSpokeVote.againstVotes > againstVotes || existingSpokeVote.forVotes > forVotes
         || existingSpokeVote.abstainVotes > abstainVotes
     ) revert InvalidProposalVote();
 
-    // Save proposal vote
     spokeProposalVotes[_spokeProposalId] = ProposalVote(againstVotes, forVotes, abstainVotes);
 
     _castVote(
@@ -66,7 +105,7 @@ contract HubVotePool {
         forVotes - existingSpokeVote.forVotes,
         abstainVotes - existingSpokeVote.abstainVotes
       ),
-      wormholeMessage.emitterChainId
+      perChainResp.chainId
     );
   }
 
---
+++ test/helpers/WormholeEthQueryTest.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: Apache 2
+pragma solidity ^0.8.23;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {Implementation} from "wormhole/Implementation.sol";
+import {Setup} from "wormhole/Setup.sol";
+import {Wormhole} from "wormhole/Wormhole.sol";
+import {QueryResponse} from "wormhole/query/QueryResponse.sol";
+
+import {SpokeMetadataCollectorQueriesHarness} from "test/harnesses/SpokeMetadataCollectorHarness.sol";
+
+contract WormholeEthQueryTest is Test {
+  address constant DEVNET_GUARDIAN = 0xbeFA429d57cD18b7F8A4d91A2da9AB4AF05d0FBe;
+  uint256 constant DEVNET_GUARDIAN_PRIVATE_KEY = 0xcfb12303a19cde580bb4dd771639b0d26bc68353645571a8cff516ab2ee113a0;
+  uint256 constant MAINNET_CHAIN_ID = 2;
+  uint8 constant VERSION = 1;
+  uint8 constant OFF_CHAIN_SENDER = 0;
+  bytes constant OFF_CHAIN_SIGNATURE =
+    hex"0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+  uint256 constant GOVERNANCE_CHAIN_ID = 2;
+  bytes signature =
+    hex"ff0c222dc9e3655ec38e212e9792bf1860356d1277462b6bf747db865caca6fc08e6317b64ee3245264e371146b1d315d38c867fe1f69614368dc4430bb560f200";
+  address GOVERNANCE_CONTRACT = 0x8a907De47E00830a2b742db65e938a3ea1070A2E; // Pooltogether governor
+  Wormhole public wormhole;
+  SpokeMetadataCollectorQueriesHarness spokeMetadataCollector;
+
+  function _setupWormhole() internal {
+    // Deploy the Setup contract.
+    Setup setup = new Setup();
+
+    // Deploy the Implementation contract.
+    Implementation implementation = new Implementation();
+
+    address[] memory guardians = new address[](1);
+    guardians[0] = DEVNET_GUARDIAN;
+
+    // Deploy the Wormhole contract.
+    wormhole = new Wormhole(
+      address(setup),
+      abi.encodeWithSelector(
+        bytes4(keccak256("setup(address,address[],uint16,uint16,bytes32,uint256)")),
+        address(implementation),
+        guardians,
+        MAINNET_CHAIN_ID,
+        GOVERNANCE_CHAIN_ID,
+        GOVERNANCE_CONTRACT,
+        block.chainid // evm chain id
+      )
+    );
+  }
+
+  function getSignature(bytes memory response, address _getSig) internal pure returns (uint8 v, bytes32 r, bytes32 s) {
+    bytes32 responseDigest = QueryResponse(_getSig).getResponseDigest(response);
+    (v, r, s) = vm.sign(DEVNET_GUARDIAN_PRIVATE_KEY, responseDigest);
+  }
+}
