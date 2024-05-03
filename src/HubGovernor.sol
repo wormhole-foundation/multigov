67f08aab5546a8372aa7582bb90e2881076bff75
Alexander Keating <keating.dev@protonmail.com>
2024-05-03 11:42:28 -0400

    Add requirements to hub governor (#30)
    * Add flexible voting to the hub governor
    * Add whitelist for trusted addresses
    
    ---------
    
    Co-authored-by: wildmolasses <changes@gmail.com>

--- src/HubGovernor.sol
+++ src/HubGovernor.sol
@@ -1,32 +1,54 @@
 // SPDX-License-Identifier: Apache 2
 pragma solidity ^0.8.23;
 
-import {Governor} from "@openzeppelin-contracts/governance/Governor.sol";
-import {GovernorCountingSimple} from "@openzeppelin-contracts/governance/extensions/GovernorCountingSimple.sol";
-import {GovernorVotes} from "@openzeppelin-contracts/governance/extensions/GovernorVotes.sol";
-import {IVotes} from "@openzeppelin-contracts/governance/utils/IVotes.sol";
-import {GovernorTimelockControl} from "@openzeppelin-contracts/governance/extensions/GovernorTimelockControl.sol";
-import {TimelockController} from "@openzeppelin-contracts/governance/TimelockController.sol";
-import {GovernorSettings} from "@openzeppelin-contracts/governance/extensions/GovernorSettings.sol";
+import {Governor} from "@openzeppelin/contracts/governance/Governor.sol";
+import {GovernorCountingSimple} from "@openzeppelin/contracts/governance/extensions/GovernorCountingSimple.sol";
+import {GovernorVotes} from "@openzeppelin/contracts/governance/extensions/GovernorVotes.sol";
+import {IVotes} from "@openzeppelin/contracts/governance/utils/IVotes.sol";
+import {GovernorTimelockControl} from "@openzeppelin/contracts/governance/extensions/GovernorTimelockControl.sol";
+import {TimelockController} from "@openzeppelin/contracts/governance/TimelockController.sol";
+import {GovernorSettings} from "@openzeppelin/contracts/governance/extensions/GovernorSettings.sol";
+import {GovernorCountingFractional} from "flexible-voting/GovernorCountingFractional.sol";
+import {SafeCast} from "@openzeppelin/contracts/utils/math/SafeCast.sol";
 
-// TODO: Add switch in Flexible Voting
-contract HubGovernor is Governor, GovernorSettings, GovernorCountingSimple, GovernorVotes, GovernorTimelockControl {
+import {GovernorSettableFixedQuorum} from "src/extensions/GovernorSettableFixedQuorum.sol";
+
+contract HubGovernor is
+  GovernorCountingFractional,
+  GovernorSettings,
+  GovernorVotes,
+  GovernorTimelockControl,
+  GovernorSettableFixedQuorum
+{
+  mapping(address votingAddress => bool enabled) public trustedVotingAddresses;
+
   constructor(
     string memory _name,
     IVotes _token,
     TimelockController _timelock,
     uint48 _initialVotingDelay,
     uint32 _initialVotingPeriod,
-    uint256 _initialProposalThreshold
+    uint256 _initialProposalThreshold,
+    uint208 _initialQuorum,
+    address _trustedVotingAddress
   )
     Governor(_name)
     GovernorSettings(_initialVotingDelay, _initialVotingPeriod, _initialProposalThreshold)
     GovernorVotes(_token)
     GovernorTimelockControl(_timelock)
-  {}
+    GovernorSettableFixedQuorum(_initialQuorum)
+  {
+    _enableTrustedVotingAddress(_trustedVotingAddress);
+  }
 
-  function quorum(uint256) public pure override returns (uint256) {
-    return 1;
+  function enableTrustedVotingAddress(address _trustedAddress) external {
+    _checkGovernance();
+    _enableTrustedVotingAddress(_trustedAddress);
+  }
+
+  function disableTrustedVotingAddress(address _trustedAddress) external {
+    _checkGovernance();
+    _disableTrustedVotingAddress(_trustedAddress);
   }
 
   function _cancel(address[] memory targets, uint256[] memory values, bytes[] memory calldatas, bytes32 descriptionHash)
@@ -38,14 +60,22 @@ contract HubGovernor is Governor, GovernorSettings, GovernorCountingSimple, Gove
     return GovernorTimelockControl._cancel(targets, values, calldatas, descriptionHash);
   }
 
+  function _disableTrustedVotingAddress(address _trustedAddress) internal {
+    trustedVotingAddresses[_trustedAddress] = false;
+  }
+
+  function _enableTrustedVotingAddress(address _trustedAddress) internal {
+    trustedVotingAddresses[_trustedAddress] = true;
+  }
+
   function _executeOperations(
-    uint256, /* proposalId */
-    address[] memory targets,
-    uint256[] memory values,
-    bytes[] memory calldatas,
-    bytes32 /*descriptionHash*/
+    uint256 _proposalId,
+    address[] memory _targets,
+    uint256[] memory _values,
+    bytes[] memory _calldatas,
+    bytes32 _descriptionHash
   ) internal virtual override(Governor, GovernorTimelockControl) {
-    GovernorTimelockControl._executeOperations(0, targets, values, calldatas, 0);
+    GovernorTimelockControl._executeOperations(_proposalId, _targets, _values, _calldatas, _descriptionHash);
   }
 
   function _executor() internal view virtual override(Governor, GovernorTimelockControl) returns (address) {
@@ -84,5 +114,21 @@ contract HubGovernor is Governor, GovernorSettings, GovernorCountingSimple, Gove
 
   function proposalThreshold() public view virtual override(Governor, GovernorSettings) returns (uint256) {
     return GovernorSettings.proposalThreshold();
+  }
+
+  function _countVote(uint256 proposalId, address account, uint8 support, uint256 totalWeight, bytes memory voteData)
+    internal
+    virtual
+    override(Governor, GovernorCountingFractional)
+  {
+    if (!trustedVotingAddresses[account]) {
+      require(totalWeight > 0, "GovernorCountingFractional: no weight");
+      if (voteWeightCast(proposalId, account) >= totalWeight) revert("GovernorCountingFractional: all weight cast");
+    }
+
+    uint128 safeTotalWeight = SafeCast.toUint128(totalWeight);
+
+    if (voteData.length == 0) _countVoteNominal(proposalId, account, safeTotalWeight, support);
+    else _countVoteFractional(proposalId, account, safeTotalWeight, voteData);
   }
 }
---
+++ test/harnesses/HubGovernorHarness.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: Apache 2
+pragma solidity ^0.8.23;
+
+import {TimelockController} from "@openzeppelin/contracts/governance/TimelockController.sol";
+import {IVotes} from "@openzeppelin/contracts/governance/utils/IVotes.sol";
+
+import {HubGovernor} from "src/HubGovernor.sol";
+
+contract HubGovernorHarness is HubGovernor {
+  constructor(
+    string memory _name,
+    IVotes _token,
+    TimelockController _timelock,
+    uint48 _initialVotingDelay,
+    uint32 _initialVotingPeriod,
+    uint256 _initialProposalThreshold,
+    uint208 _initialQuorum,
+    address _hubVotePool
+  )
+    HubGovernor(
+      _name,
+      _token,
+      _timelock,
+      _initialVotingDelay,
+      _initialVotingPeriod,
+      _initialProposalThreshold,
+      _initialQuorum,
+      _hubVotePool
+    )
+  {}
+
+  function exposed_setQuorum(uint208 _amount) public {
+    _setQuorum(_amount);
+  }
+
+  function exposed_countVote(
+    uint256 _proposalId,
+    address _account,
+    uint8 _support,
+    uint256 _totalWeight,
+    bytes memory _voteData
+  ) public {
+    _countVote(_proposalId, _account, _support, _totalWeight, _voteData);
+  }
+
+  function exposed_enableTrustedAddress(address _trustedAddress) public {
+    _enableTrustedVotingAddress(_trustedAddress);
+  }
+}
