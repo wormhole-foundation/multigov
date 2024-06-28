525f2071de52d101321e6adf10babb3aa50e0a47
zpoken <82594516+zpoken@users.noreply.github.com>
2024-06-28 13:18:07 +0300

    Add get_past_votes and test_get_past_votes

--- solana/programs/staking/src/utils/voter_votes.rs
+++ solana/programs/staking/src/utils/voter_votes.rs
@@ -15,6 +15,15 @@ pub fn get_votes(
     Ok(votes)
 }
 
+pub fn get_past_votes(
+    stake_account_checkpoints: &CheckpointData,
+    timestamp: u64
+) -> Result<u64> {
+    let votes: u64 = stake_account_checkpoints.get_at_probably_recent_timestamp(timestamp)?
+        .unwrap_or(0);
+    Ok(votes)
+}
+
 #[cfg(test)]
 pub mod tests {
     use {
@@ -22,7 +31,10 @@ pub mod tests {
             state::checkpoints::{
                 CheckpointData,
             },
-            utils::voter_votes::get_votes,
+            utils::voter_votes::{
+                get_votes,
+                get_past_votes,
+            }
         },
     };
 
@@ -30,15 +42,47 @@ pub mod tests {
     fn test_get_votes() {
         let mut checkpointData = CheckpointData::default();
 
+        let mut votes = get_votes(&checkpointData).unwrap();
+        assert_eq!(votes, 0);
+
         checkpointData.push(1, 7).unwrap();
         checkpointData.push(2, 13).unwrap();
 
-        let mut votes = get_votes(&checkpointData).unwrap();
+        votes = get_votes(&checkpointData).unwrap();
         assert_eq!(votes, 13);
 
         checkpointData.push(3, 15).unwrap();
 
         votes = get_votes(&checkpointData).unwrap();
         assert_eq!(votes, 15);
+    }
+
+    #[test]
+    fn test_get_past_votes() {
+        let mut checkpointData = CheckpointData::default();
+
+        let mut votes = get_past_votes(&checkpointData, 100).unwrap();
+        assert_eq!(votes, 0);
+
+        checkpointData.push(1, 7).unwrap();
+        checkpointData.push(3, 13).unwrap();
+        checkpointData.push(5, 16).unwrap();
+        checkpointData.push(10, 17).unwrap();
+        checkpointData.push(12, 20).unwrap();
+        checkpointData.push(20, 34).unwrap();
+
+        votes = get_past_votes(&checkpointData, 8).unwrap();
+        assert_eq!(votes, 16);
+        votes = get_past_votes(&checkpointData, 12).unwrap();
+        assert_eq!(votes, 20);
+
+        checkpointData.push(21, 19).unwrap();
+        checkpointData.push(21, 50).unwrap();
+        checkpointData.push(25, 44).unwrap();
+
+        votes = get_past_votes(&checkpointData, 21).unwrap();
+        assert_eq!(votes, 50);
+        votes = get_past_votes(&checkpointData, 26).unwrap();
+        assert_eq!(votes, 44);
     }
 }
