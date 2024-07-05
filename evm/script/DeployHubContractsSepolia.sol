496abda54ec5d8608dc34e5cb1e83c0a97ae3eed
keating <keating.dev@protonmail.com>
2024-07-05 14:04:20 -0400

    Another testnet hub deploy

--- evm/script/DeployHubContractsSepolia.sol
+++ evm/script/DeployHubContractsSepolia.sol
@@ -11,16 +11,16 @@ contract DeployHubContractsSepolia is DeployHubContractsBaseImpl {
       minDelay: 300,
       name: "Wormhole Sepolia Governor",
       token: 0x4b56814a4A5b38De8406F3E04F5b39628658cD1B,
-      initialVotingDelay: 90,
-      initialVotingPeriod: 1800,
+      initialVotingDelay: 1.5 minutes,
+      initialVotingPeriod: 30 minutes,
       initialProposalThreshold: 500_000e18,
       initialQuorum: 1_000_000e18,
       wormholeCore: 0x31377888146f3253211EFEf5c676D41ECe7D58Fe,
       voteWeightWindow: 10 minutes,
       whitelistedVoteExtender: wallet.addr,
-      voteTimeExtension: 30,
-      minimumDecisionWindow: 1,
-      minimumExtensionTime: 1
+      voteTimeExtension: 5 minutes,
+      minimumDecisionWindow: 5 minutes,
+      minimumExtensionTime: 1 minutes
     });
   }
 }
