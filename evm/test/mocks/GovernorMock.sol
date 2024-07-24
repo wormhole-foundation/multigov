1d37a858cef67c57945e20f5789bf831cdc49244
marcomariscal <marco.a.mariscal@gmail.com>
2024-07-24 11:53:29 -0700

    chore: remove unused

--- evm/test/mocks/GovernorMock.sol
+++ evm/test/mocks/GovernorMock.sol
@@ -8,10 +8,6 @@ contract GovernorMock {
   uint8 public support;
   string public reason;
   bytes public params;
-  uint256 voteStart;
-  uint256 public proposalThreshold;
-  address public whitelistedProposer;
-  ERC20VotesFake public token;
 
   function castVoteWithReasonAndParams(
     uint256 _proposalId,
@@ -24,34 +20,5 @@ contract GovernorMock {
     reason = _reason;
     params = _params;
     return _proposalId;
-  }
-
-  function propose(address[] memory, uint256[] memory, bytes[] memory, string memory) public virtual returns (uint256) {
-    voteStart = block.timestamp;
-    return voteStart;
-  }
-
-  function proposalSnapshot(uint256) public view virtual returns (uint256) {
-    return voteStart;
-  }
-
-  function votingPeriod() public pure returns (uint256) {
-    return 7 days;
-  }
-
-  function setProposalThreshold(uint256 _proposalThreshold) public virtual {
-    proposalThreshold = _proposalThreshold;
-  }
-
-  function setWhitelistedProposer(address _proposer) public virtual {
-    whitelistedProposer = _proposer;
-  }
-
-  function setToken(address _token) external {
-    token = ERC20VotesFake(_token);
-  }
-
-  function getVotes(address account, uint256 blockNumber) public view returns (uint256) {
-    return token.getPastVotes(account, blockNumber);
   }
 }
