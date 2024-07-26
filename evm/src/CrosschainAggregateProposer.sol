de859509b38e7e92d2b6501e8c1e22980201a22a
marcomariscal <marco.a.mariscal@gmail.com>
2024-07-25 20:11:44 -0700

    chore: remove unused import

--- evm/src/CrosschainAggregateProposer.sol
+++ evm/src/CrosschainAggregateProposer.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: Apache 2
 pragma solidity ^0.8.23;
 
-import {console} from "forge-std/Test.sol";
 import {IGovernor} from "@openzeppelin/contracts/governance/IGovernor.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {IWormhole} from "wormhole/interfaces/IWormhole.sol";
