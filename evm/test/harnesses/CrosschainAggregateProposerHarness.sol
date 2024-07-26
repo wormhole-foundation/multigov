827d2eef6879a501cee8cfabe98598f7e0b1ad0b
marcomariscal <marco.a.mariscal@gmail.com>
2024-07-25 20:37:36 -0700

    fix: _maxQueryTimestampOffset

--- evm/test/harnesses/CrosschainAggregateProposerHarness.sol
+++ evm/test/harnesses/CrosschainAggregateProposerHarness.sol
@@ -4,8 +4,8 @@ pragma solidity ^0.8.23;
 import {CrossChainAggregateProposer} from "src/CrossChainAggregateProposer.sol";
 
 contract CrossChainAggregateProposerHarness is CrossChainAggregateProposer {
-  constructor(address _core, address _hubGovernor, uint48 _initialMinAllowedTimeDelta)
-    CrossChainAggregateProposer(_core, _hubGovernor, _initialMinAllowedTimeDelta)
+  constructor(address _core, address _hubGovernor, uint48 _maxQueryTimestampOffset)
+    CrossChainAggregateProposer(_core, _hubGovernor, _maxQueryTimestampOffset)
   {}
 
   function exposed_extractAccountFromCalldata(bytes memory callData) public pure returns (address) {
