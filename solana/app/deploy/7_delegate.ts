f6bc79a5822271f28c23b669b484094d02881bef
zpoken <82594516+zpoken@users.noreply.github.com>
2024-07-26 10:49:24 +0300

    Add script to delegate in devnet

---
+++ solana/app/deploy/7_delegate.ts
@@ -0,0 +1,46 @@
+import * as anchor from "@coral-xyz/anchor";
+import { AnchorProvider, Wallet, } from "@coral-xyz/anchor";
+import { Connection } from "@solana/web3.js";
+import { StakeConnection } from "../StakeConnection";
+import { WHTokenBalance } from "../whTokenBalance";
+import { STAKING_ADDRESS } from "../constants";
+import { USER_AUTHORITY_KEYPAIR, RPC_NODE } from "./devnet";
+
+async function main() {
+  try {
+    const connection = new Connection(RPC_NODE);
+
+    const provider = new AnchorProvider(
+      connection,
+      new Wallet(USER_AUTHORITY_KEYPAIR),
+      {}
+    );
+
+    const stakeConnection = await StakeConnection.createStakeConnection(
+      connection,
+      provider.wallet as Wallet,
+      STAKING_ADDRESS
+    );
+
+    await stakeConnection.delegate(
+      undefined,
+      undefined,
+      WHTokenBalance.fromString("100")
+    );
+
+    const user = provider.wallet.publicKey;
+    const stakeAccount = await stakeConnection.getMainAccount(user);
+    console.log(stakeAccount)
+
+    await stakeConnection.delegate(
+      stakeAccount,
+      stakeAccount,
+      WHTokenBalance.fromString("100")
+    );
+  } catch (err) {
+    console.error("Error:", err);
+  }
+}
+
+main();
+
