60d0c9387ec50652d5b9cbc66eb545f13921e847
Alexander Keating <keating.dev@protonmail.com>
2024-07-30 16:03:41 -0400

    Follow up to hub vote pool refactor (#113)

--- evm/src/interfaces/ICrossChainVoteDecoder.sol
+++ evm/src/interfaces/ICrossChainVoteDecoder.sol
@@ -5,7 +5,7 @@ import {ParsedPerChainQueryResponse} from "wormhole/query/QueryResponse.sol";
 import {IERC165} from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 import {IWormhole} from "wormhole/interfaces/IWormhole.sol";
 
-interface ICrossChainVote is IERC165 {
+interface ICrossChainVoteDecoder is IERC165 {
   struct ProposalVote {
     uint128 againstVotes;
     uint128 forVotes;
@@ -23,5 +23,5 @@ interface ICrossChainVote is IERC165 {
   error UnknownMessageEmitter();
   error InvalidProposalVote();
 
-  function crossChainVote(ParsedPerChainQueryResponse memory _queryResponse) external returns (QueryVote memory);
+  function decode(ParsedPerChainQueryResponse memory _queryResponse) external returns (QueryVote memory);
 }
