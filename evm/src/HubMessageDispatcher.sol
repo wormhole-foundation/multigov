649a76e44600bfd06cb6b221e24610711b91c843
Alexander Keating <keating.dev@protonmail.com>
2024-07-30 16:03:21 -0400

    Add dispatcher check (#108)

--- evm/src/HubMessageDispatcher.sol
+++ evm/src/HubMessageDispatcher.sol
@@ -4,9 +4,11 @@ pragma solidity ^0.8.23;
 import {WormholeDispatcher} from "src/WormholeDispatcher.sol";
 
 contract HubMessageDispatcher is WormholeDispatcher {
-  event MessageDispatched(uint256 indexed proposalId, bytes payload);
-
   uint256 public nextMessageId = 1;
+
+  error InvalidSpokeExecutorOperationLength(uint256, uint256, uint256);
+
+  event MessageDispatched(uint256 indexed proposalId, bytes payload);
 
   constructor(address _timelock, address _core, uint8 _dispatchConsistencyLevel)
     WormholeDispatcher(_timelock, _core, _dispatchConsistencyLevel)
@@ -17,6 +19,10 @@ contract HubMessageDispatcher is WormholeDispatcher {
 
     (uint16 wormholeChainId, address[] memory targets, uint256[] memory values, bytes[] memory calldatas) =
       abi.decode(_payload, (uint16, address[], uint256[], bytes[]));
+
+    if (targets.length != values.length || targets.length != calldatas.length) {
+      revert InvalidSpokeExecutorOperationLength(targets.length, values.length, calldatas.length);
+    }
 
     bytes memory payload = abi.encode(nextMessageId, wormholeChainId, targets, values, calldatas);
     _publishMessage(payload);
--- evm/test/HubMessageDispatcher.t.sol
+++ evm/test/HubMessageDispatcher.t.sol
@@ -14,7 +14,7 @@ contract HubMessageDispatcherTest is Test, TestConstants {
   HubMessageDispatcher dispatcher;
   WormholeCoreMock wormholeCoreMock;
 
-  function setUp() public {
+  function setUp() public virtual {
     wormholeCoreMock = new WormholeCoreMock();
     TimelockControllerFake timelock = TimelockControllerFake(payable(address(this)));
     dispatcher = new HubMessageDispatcher(address(timelock), address(wormholeCoreMock), 0);
@@ -36,35 +36,88 @@ contract Constructor is HubMessageDispatcherTest {
 }
 
 contract Dispatch is HubMessageDispatcherTest {
-  function testFuzz_CorrectlyEncodeProposalPayload(
-    address[] memory _targets,
-    uint256[] memory _values,
-    bytes[] memory _calldatas,
+  ProposalBuilder builder = new ProposalBuilder();
+
+  function setUp() public override {
+    super.setUp();
+    builder = new ProposalBuilder();
+  }
+
+  function _addCrossChainCall(bytes memory _callData) public {
+    builder.push(makeAddr("spokeContractCall"), 0, _callData);
+  }
+
+  function testFuzz_CorrectlyEncodeProposalSinglePayload(
+    uint32 _votingPeriod,
     string memory _description,
     uint16 _wormholeChainId
   ) public {
+    _addCrossChainCall(abi.encodeWithSignature("setVotingPeriod(uint32)", _votingPeriod));
     uint256 nextMessageId = dispatcher.nextMessageId();
-    bytes memory payload = abi.encode(_wormholeChainId, _targets, _values, _calldatas, keccak256(bytes(_description)));
+    bytes memory payload = abi.encode(
+      _wormholeChainId, builder.targets(), builder.values(), builder.calldatas(), keccak256(bytes(_description))
+    );
     dispatcher.dispatch(payload);
     assertEq(
       wormholeCoreMock.ghostPublishMessagePayload(),
-      abi.encode(nextMessageId, _wormholeChainId, _targets, _values, _calldatas)
+      abi.encode(nextMessageId, _wormholeChainId, builder.targets(), builder.values(), builder.calldatas())
+    );
+  }
+
+  function testFuzz_CorrectlyEncodeProposalMultiplePayload(
+    uint32 _votingPeriod,
+    string memory _description,
+    uint16 _wormholeChainId
+  ) public {
+    _addCrossChainCall(abi.encodeWithSignature("setVotingPeriod(uint32)", _votingPeriod));
+    _addCrossChainCall(abi.encodeWithSignature("setVotingPeriod(uint32)", _votingPeriod));
+    uint256 nextMessageId = dispatcher.nextMessageId();
+    bytes memory payload = abi.encode(
+      _wormholeChainId, builder.targets(), builder.values(), builder.calldatas(), keccak256(bytes(_description))
+    );
+    dispatcher.dispatch(payload);
+    assertEq(
+      wormholeCoreMock.ghostPublishMessagePayload(),
+      abi.encode(nextMessageId, _wormholeChainId, builder.targets(), builder.values(), builder.calldatas())
     );
   }
 
   function testFuzz_EmitsAMessageDispatchedEvent(
+    uint32 _votingPeriod,
+    string memory _description,
+    uint16 _wormholeChainId
+  ) public {
+    _addCrossChainCall(abi.encodeWithSignature("setVotingPeriod(uint32)", _votingPeriod));
+    uint256 nextMessageId = dispatcher.nextMessageId();
+    bytes memory payload = abi.encode(
+      _wormholeChainId, builder.targets(), builder.values(), builder.calldatas(), keccak256(bytes(_description))
+    );
+    bytes memory emittedPayload =
+      abi.encode(nextMessageId, _wormholeChainId, builder.targets(), builder.values(), builder.calldatas());
+
+    vm.expectEmit();
+    emit HubMessageDispatcher.MessageDispatched(nextMessageId, emittedPayload);
+    dispatcher.dispatch(payload);
+  }
+
+  function testFuzz_RevertIf_ProposalDataIsDifferentLengths(
     address[] memory _targets,
     uint256[] memory _values,
     bytes[] memory _calldatas,
     string memory _description,
     uint16 _wormholeChainId
   ) public {
-    uint256 nextMessageId = dispatcher.nextMessageId();
+    vm.assume(_targets.length != _values.length || _calldatas.length != _targets.length);
     bytes memory payload = abi.encode(_wormholeChainId, _targets, _values, _calldatas, keccak256(bytes(_description)));
-    bytes memory emittedPayload = abi.encode(nextMessageId, _wormholeChainId, _targets, _values, _calldatas);
 
-    vm.expectEmit();
-    emit HubMessageDispatcher.MessageDispatched(nextMessageId, emittedPayload);
+    vm.expectRevert(
+      abi.encodeWithSelector(
+        HubMessageDispatcher.InvalidSpokeExecutorOperationLength.selector,
+        _targets.length,
+        _values.length,
+        _calldatas.length
+      )
+    );
     dispatcher.dispatch(payload);
   }
 }
