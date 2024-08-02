01f27dc836190f3b8af292774467c45f18d5da43
Alexander Keating <keating.dev@protonmail.com>
2024-08-02 10:57:25 -0400

    Update evm/src/HubVotePool.sol
    Co-authored-by: Ed Mazurek <changes@gmail.com>

--- evm/src/HubVotePool.sol
+++ evm/src/HubVotePool.sol
@@ -21,7 +21,7 @@ contract HubVotePool is QueryResponse, Ownable {
   /// @notice A necessary param which is ignored when submitting a vote.
   uint8 constant UNUSED_SUPPORT_PARAM = 1;
 
-  /// @notice Thrown when the submited spoke aggregator vote has a vote that is inconsistent with the previously
+  /// @notice Thrown when the submitted spoke aggregator vote has a vote that is inconsistent with the previously
   /// submitted vote.
   error InvalidProposalVote();
 
