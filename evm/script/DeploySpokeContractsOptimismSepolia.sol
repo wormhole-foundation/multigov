758a0c2d366822fb5e5860ce672c037f2f9f9238
keating <keating.dev@protonmail.com>
2024-08-02 16:46:16 -0400

    WIP

--- evm/script/DeploySpokeContractsOptimismSepolia.sol
+++ evm/script/DeploySpokeContractsOptimismSepolia.sol
@@ -8,10 +8,10 @@ contract DeploySpokeContractsOptimismSepolia is DeploySpokeContractsBaseImpl {
     return DeploymentConfiguration({
       wormholeCore: 0x31377888146f3253211EFEf5c676D41ECe7D58Fe,
       hubChainId: 10_002,
-      hubProposalMetadata: 0x0D500a8A3A20E003FD0C0bd24813C00264297E90,
+      hubProposalMetadata: 0xEe1ca373D32100B813b25402ee683667e567E50e,
       votingToken: 0x74f00907CFC6E44Fb72535cdD1eC52a37EacAbE4,
       voteWeightWindow: 10 minutes,
-      hubDispatcher: 0x000000000000000000000000a86c31eb2c8865659c6e3f119563723663a79388,
+      hubDispatcher:  0x000000000000000000000000ac3153076455acd2481fd532291b8a13c3be5d5e,
       spokeChainId: 10_005
     });
   }
