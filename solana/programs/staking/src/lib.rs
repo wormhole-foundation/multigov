a785aab81dc5612c6947cc66fa246d9fc08e14e9
zpoken <82594516+zpoken@users.noreply.github.com>
2024-08-07 18:33:02 +0300

    Add vesting program functions

--- solana/programs/staking/src/lib.rs
+++ solana/programs/staking/src/lib.rs
@@ -12,11 +12,13 @@ use anchor_spl::token::transfer;
 use context::*;
 use state::global_config::GlobalConfig;
 use std::convert::TryInto;
+use contexts::*;
 
 mod context;
 mod error;
 mod state;
 mod utils;
+mod contexts;
 #[cfg(feature = "wasm")]
 pub mod wasm;
 
@@ -326,4 +328,38 @@ pub mod staking {
 
         Ok(())
     }
+
+    //------------------------------------ VESTING ------------------------------------------------
+    // Initialize a new Config, setting up a mint, vault and admin
+    pub fn initialize_vesting_config(ctx: Context<Initialize>, seed: u64) -> Result<()> {
+        ctx.accounts.initialize(seed, ctx.bumps.config)
+    }
+
+    // Finalize a Config, disabling any further creation or cancellation of Vesting accounts
+    pub fn finalize_vesting_config(ctx: Context<Finalize>) -> Result<()> {
+        ctx.accounts.finalize()
+    }
+
+    // Open a new Vesting account and deposit equivalent vested tokens to vault
+    pub fn create_vesting(ctx: Context<CreateVesting>, maturation: i64, amount: u64) -> Result<()> {
+        ctx.accounts.create_vesting(maturation, amount, ctx.bumps.vest)
+    }
+
+    // Claim from and close a Vesting account
+    pub fn claim_vesting(ctx: Context<ClaimVesting>) -> Result<()> {
+        ctx.accounts.close_vesting()
+    }
+
+    // Cancel and close a Vesting account for a non-finalized Config
+    pub fn cancel_vesting(ctx: Context<CancelVesting>) -> Result<()> {
+        ctx.accounts.cancel_vesting()
+    }
+
+    // Allow admin to withdraw surplus tokens in excess of total vested amount
+    pub fn withdraw_surplus(ctx: Context<WithdrawSurplus>) -> Result<()> {
+        ctx.accounts.withdraw_surplus()
+    }
 }
+
+
+
