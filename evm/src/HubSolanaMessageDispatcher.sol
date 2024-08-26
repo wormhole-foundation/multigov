c37ac2d836dd56f877b45664966812cdc6ad4b3c
marcomariscal <marco.a.mariscal@gmail.com>
2024-08-26 13:14:36 -0700

    feat: wip HubSolanaMessageDispatcher

---
+++ evm/src/HubSolanaMessageDispatcher.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: Apache 2
+pragma solidity ^0.8.23;
+
+import {WormholeDispatcher} from "src/WormholeDispatcher.sol";
+
+/// @title HubSolanaMessageDispatcher
+/// @author [ScopeLift](https://scopelift.co)
+/// @notice A contract that will publish a message that can be relayed to the appropriate `SpokeExecutor` on Solana.
+contract HubSolanaMessageDispatcher is WormholeDispatcher {
+  /// @notice The id for the next message published.
+  /// @dev This value is incremented after each successful message dispatch.
+  uint256 public nextMessageId = 1;
+
+  uint8 immutable SOLANA_WORMHOLE_CHAIN_ID = 1;
+
+  /// @notice Thrown if the encoded payload is invalid.
+  error InvalidSolanaOperationLength(uint256 instructionsLength, uint256 accountsLength);
+
+  /// @notice Emitted when a message is dispatched.
+  event MessageDispatched(uint256 indexed messageId, bytes payload);
+
+  /// @notice Thrown if the chain id is not Solana.
+  error InvalidChainId();
+
+  /// @param _timelock The timelock that will call the hub dispatcher to initiate a cross chain execution.
+  /// @param _core The Wormhole contract that will handle publishing the cross chain message.
+  /// @param _dispatchConsistencyLevel The consistency level of a message when sending it to another chain. In most
+  /// situations this should be set to finalized.
+  constructor(address _timelock, address _core, uint8 _dispatchConsistencyLevel)
+    WormholeDispatcher(_timelock, _core, _dispatchConsistencyLevel)
+  {}
+
+  struct SolanaAccountMeta {
+    bytes32 pubkey;
+    bool isSigner;
+    bool isWritable;
+  }
+
+  struct SolanaInstruction {
+    bytes32 programId;
+    SolanaAccountMeta[] accounts;
+    bytes data;
+  }
+
+  function dispatch(bytes calldata _payload) external {
+    _checkOwner();
+
+    (uint16 _wormholeChainId, SolanaInstruction[] memory instructions) =
+      abi.decode(_payload, (uint16, SolanaInstruction[]));
+
+    if (_wormholeChainId != SOLANA_WORMHOLE_CHAIN_ID) revert InvalidChainId();
+
+    for (uint256 i = 0; i < instructions.length; i++) {
+      if (instructions[i].accounts.length == 0) {
+        revert InvalidSolanaOperationLength(instructions.length, instructions[i].accounts.length);
+      }
+    }
+
+    bytes memory message = abi.encode(nextMessageId, _wormholeChainId, instructions);
+    _publishMessage(message);
+
+    emit MessageDispatched(nextMessageId, message);
+    nextMessageId++;
+  }
+}
