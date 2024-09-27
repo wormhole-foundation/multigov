4f9b74db7301fa8aa90c133e8a75ce8836841bb6
keating <keating.dev@protonmail.com>
2024-09-27 14:07:46 -0400

    WIP: support bytes32 for registry

--- evm/src/HubSpokeRegistry.sol
+++ evm/src/HubSpokeRegistry.sol
@@ -1,20 +1,32 @@
 // SPDX-License-Identifier: Apache 2
 pragma solidity ^0.8.23;
 
+import {Checkpoints} from "src/lib/Checkpoints.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract HubSpokeRegistry {
+  using Checkpoints for Checkpoints.Trace256;
+
   /// @notice A mapping of registered spoke aggregators per chain. These chains and addresses determine the chains and
   /// addresses that can be queried in order to aggregate voting weight.
-  mapping(uint16 wormholeChainId => address spokeVoteAggregator) public registeredSpokes;
+  mapping(uint16 wormholeChainId => Checkpoints.Trace256 spokeVoteAggregator) internal emitterRegistry;
 
   /// @notice Emitted when a spoke is registered.
-  event SpokeRegistered(uint16 indexed chainId, address oldSpokeAddress, address newSpokeAddress);
+  event SpokeRegistered(uint16 indexed chainId, bytes32 oldSpokeAddress, bytes32 newSpokeAddress);
 
 
-  function registerSpoke(uint16 _chainId, address _spokeVoteAggregator) external {
-    emit SpokeRegistered(_chainId, registeredSpokes[_chainId], _spokeVoteAggregator);
-    registeredSpokes[_chainId] = _spokeVoteAggregator;
+  function registerSpoke(uint16 _chainId, bytes32 _spokeVoteAggregator) external {
+    Checkpoints.Trace256 storage registeredAddressCheckpoint = emitterRegistry[_chainId];
+    emit SpokeRegistered(
+      _chainId, bytes32(registeredAddressCheckpoint.upperLookup(block.timestamp)), _spokeVoteAggregator
+	);
+    registeredAddressCheckpoint.push(block.timestamp, uint256(_spokeVoteAggregator));
   }
+
+  function getSpoke(uint16 _emitterChainId, uint256 _timepoint) external view returns (bytes32) {
+    return bytes32(emitterRegistry[_emitterChainId].upperLookup(_timepoint));
+  }
+
+
 
 }
