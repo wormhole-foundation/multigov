982b28e627649ac6c7f435d08c1150fe6700e5a1
Alexander Keating <keating.dev@protonmail.com>
2024-10-03 11:08:42 -0400

    Add validation on proposal extender in the Governor (#179)

--- evm/test/HubSolanaSpokeVoteDecoder.t.sol
+++ evm/test/HubSolanaSpokeVoteDecoder.t.sol
@@ -49,7 +49,7 @@ contract HubSolanaSpokeVoteDecoderTest is WormholeEthQueryTest, AddressUtils {
     address hubVotePoolOwner = address(timelock);
     token = new ERC20VotesFake();
 
-    extender = new HubProposalExtender(initialOwner, VOTE_TIME_EXTENSION, initialOwner, MINIMUM_VOTE_EXTENSION);
+    extender = new HubProposalExtender(initialOwner, VOTE_TIME_EXTENSION, address(timelock), MINIMUM_VOTE_EXTENSION);
 
     HubGovernor.ConstructorParams memory params = HubGovernor.ConstructorParams({
       name: "Test Gov",
