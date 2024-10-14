e7087accbb29dcd7fa4c38f2ad2559afc60579f8
marcomariscal <marco.a.mariscal@gmail.com>
2024-10-14 10:36:44 -0700

    chore: remove unused

--- evm/src/HubSolanaSpokeVoteDecoder.sol
+++ evm/src/HubSolanaSpokeVoteDecoder.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: Apache 2
 pragma solidity ^0.8.23;
 
-import {console} from "forge-std/Test.sol";
 import {IGovernor} from "@openzeppelin/contracts/governance/IGovernor.sol";
 import {QueryResponse} from "wormhole-sdk/QueryResponse.sol";
 import {ERC165} from "@openzeppelin/contracts/utils/introspection/ERC165.sol";
