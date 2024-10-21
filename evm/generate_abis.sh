ddd7fd4d4911796bf96e94bedb70da8b12f85382
marcomariscal <marco.a.mariscal@gmail.com>
2024-10-21 12:26:54 -0700

    feat: script to output abis

---
+++ evm/generate_abis.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# List of main contracts
+main_contracts=(
+    "HubEvmSpokeAggregateProposer"
+    "HubEvmSpokeVoteDecoder"
+    "HubGovernor"
+    "HubMessageDispatcher"
+    "HubProposalExtender"
+    "HubProposalMetadata"
+    "HubSolanaMessageDispatcher"
+    "HubSolanaSpokeVoteDecoder"
+    "HubVotePool"
+    "SpokeAirlock"
+    "SpokeMessageExecutor"
+    "SpokeMetadataCollector"
+    "SpokeVoteAggregator"
+    "WormholeDispatcher"
+)
+
+# Create a directory for the ABIs if it doesn't exist
+mkdir -p abis
+
+# Loop through each main contract and generate its ABI
+for contract in "${main_contracts[@]}"
+do
+    forge inspect $contract abi > abis/${contract}_abi.json
+    echo "Generated ABI for $contract"
+done
+
+echo "ABIs for main contracts generated in the 'abis' directory."
