61abaf1b92ddab11532476b85b1df52c29fa10b0
marcomariscal <marco.a.mariscal@gmail.com>
2024-10-29 09:49:20 -0700

    feat: get proposal data

--- integration-tests/test/createProposalFromHub/index.ts
+++ integration-tests/test/createProposalFromHub/index.ts
@@ -1,5 +1,6 @@
 import { afterAll, beforeAll, describe, expect, test } from 'bun:test';
 import { ContractAddresses } from 'test/config/addresses';
+import { getProposal } from 'test/helpers/governance/proposalHelpers';
 import { getWhitelistedProposer } from 'test/helpers/governance/registrationHelpers';
 import { setupTestEnvironment, teardownTestEnvironment } from '../setup';
 import { createProposalFromHub } from './helpers';
@@ -22,5 +23,13 @@ describe('Create proposal from hub', () => {
   test('should create proposal from hub', async () => {
     const proposalId = await createProposalFromHub();
     expect(proposalId).toBeDefined();
+
+    // check it exists in the governor
+    const proposal = await getProposal(proposalId);
+    expect(proposal).toBeDefined();
+    expect(proposal.id).toBe(proposalId);
+    expect(proposal.proposer).toBe(
+      ContractAddresses.HUB_EVM_SPOKE_AGGREGATE_PROPOSER,
+    );
   });
 });
