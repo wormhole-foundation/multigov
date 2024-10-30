b0e11dc5a518ac83fee6014a8b58eb4543482c0a
marcomariscal <marco.a.mariscal@gmail.com>
2024-10-30 11:16:00 -0700

    feat: handle bridging votes to hub

--- integration-tests/test/voteFromSpoke/helpers.ts
+++ integration-tests/test/voteFromSpoke/helpers.ts
@@ -172,6 +172,112 @@ export const waitForProposalActive = async (proposalId: bigint) => {
   console.log('✅ Proposal is active');
 };
 
-export const bridgeVotesToHub = async () => {
-  // TODO: Add the rest of the logic to bridge votes to hub
+// Votes on the spoke via the `SpokeVoteAggregator` contract
+export const voteOnSpoke = async (proposalId: bigint) => {
+  const { eth2Client, eth2Wallet, account } = createClients();
+
+  await eth2Client.simulateContract({
+    address: ContractAddresses.SPOKE_VOTE_AGGREGATOR,
+    abi: SpokeVoteAggregatorAbi,
+    functionName: 'castVote',
+    args: [proposalId, VoteType.FOR],
+    account,
+  });
+
+  const hash = await eth2Wallet.writeContract({
+    address: ContractAddresses.SPOKE_VOTE_AGGREGATOR,
+    abi: SpokeVoteAggregatorAbi,
+    functionName: 'castVote',
+    args: [proposalId, VoteType.FOR],
+    account,
+  });
+
+  const receipt = await eth2Client.waitForTransactionReceipt({ hash });
+
+  console.log('voted on spoke');
+
+  return receipt.blockNumber;
+};
+
+export const bridgeVotesToHub = async (
+  proposalId: bigint,
+  votedAtBlock: bigint,
+) => {
+  const { ethClient, ethWallet, account } = createClients();
+
+  const { queryResponseBytes, queryResponseSignatures } =
+    await getWormholeProposalVotesQueryResponse({
+      proposalId,
+      votedAtBlock,
+    });
+
+  // Submit the spoke votes to the hub vote pool
+  await ethClient.simulateContract({
+    address: ContractAddresses.HUB_VOTE_POOL,
+    abi: HubVotePoolAbi,
+    functionName: 'crossChainVote',
+    args: [queryResponseBytes, queryResponseSignatures],
+    account,
+  });
+
+  const hash = await ethWallet.writeContract({
+    address: ContractAddresses.HUB_VOTE_POOL,
+    abi: HubVotePoolAbi,
+    functionName: 'crossChainVote',
+    args: [queryResponseBytes, queryResponseSignatures],
+    account,
+  });
+
+  await ethClient.waitForTransactionReceipt({ hash });
+
+  console.log('bridged votes to hub');
+};
+
+export const getWormholeProposalVotesQueryResponse = async ({
+  proposalId,
+  votedAtBlock,
+}: {
+  proposalId: bigint;
+  votedAtBlock: bigint;
+}) => {
+  const blockNumberHex = `0x${votedAtBlock.toString(16)}`;
+
+  const spokeProposalVotesCall: EthCallData = {
+    to: ContractAddresses.SPOKE_VOTE_AGGREGATOR,
+    data: encodeFunctionData({
+      abi: SpokeVoteAggregatorAbi,
+      functionName: 'proposalVotes',
+      args: [proposalId],
+    }),
+  };
+
+  const spokeQuery = new EthCallWithFinalityQueryRequest(
+    blockNumberHex,
+    'finalized',
+    [spokeProposalVotesCall],
+  );
+
+  const spokeChainQuery = new PerChainQueryRequest(
+    ETH2_DEVNET_WORMHOLE_CHAIN_ID,
+    spokeQuery,
+  );
+
+  const nonce = 1;
+  const request = new QueryRequest(nonce, [spokeChainQuery]);
+
+  // Serialize the request
+  const serialized = request.serialize();
+
+  const privateKeyStr = getPrivateKeyHex().slice(2);
+
+  // Sign the request
+  const signature = sign(
+    privateKeyStr,
+    QueryRequest.digest('DEVNET', serialized),
+  );
+
+  return await sendQueryToWormhole({
+    serialized,
+    signature,
+  });
 };
--- integration-tests/test/voteFromSpoke/index.ts
+++ integration-tests/test/voteFromSpoke/index.ts
@@ -1,26 +1,43 @@
-import { afterAll, beforeAll, describe, test } from 'bun:test';
-import { createProposalOnSpoke } from 'test/createProposalOnSpoke/helpers';
+import { afterAll, beforeAll, describe, expect, test } from 'bun:test';
 import { setupTestEnvironment, teardownTestEnvironment } from '../setup';
-import { bridgeVotesToHub, voteFromSpoke } from './helpers';
+import { voteFromSpoke } from './helpers';
+import { getProposalVotes, getVotingPower } from 'test/helpers';
+import { createClients } from 'test/config/clients';
+import { createProposalOnSpoke } from 'test/createProposalOnSpoke/helpers';
 
 describe('Vote from spoke', () => {
-  let proposalId: bigint;
-
   beforeAll(async () => {
     await setupTestEnvironment();
-    proposalId = await createProposalOnSpoke();
-    console.log('🦄 ~ beforeAll ~ proposalId:', proposalId);
   });
 
   afterAll(async () => {
     await teardownTestEnvironment();
   });
 
-  test('should successfully vote from spoke', async () => {
-    await voteFromSpoke();
-  });
+  test('should successfully vote from spoke and bridge to hub', async () => {
+    const { account, ethClient } = createClients();
+    const voteWeight = await getVotingPower({
+      account: account.address,
+      isHub: true,
+      timestamp: (await ethClient.getBlock()).timestamp,
+    });
 
-  test('should successfully bridge votes to hub', async () => {
-    await bridgeVotesToHub();
+    const proposalId = await createProposalOnSpoke();
+
+    const votesBeforeOnHub = await getProposalVotes({
+      proposalId,
+      isHub: true,
+    });
+
+    await voteFromSpoke(proposalId);
+
+    const votesAfterOnHub = await getProposalVotes({
+      proposalId,
+      isHub: true,
+    });
+
+    expect(votesAfterOnHub.forVotes).toBe(
+      votesBeforeOnHub.forVotes + voteWeight,
+    );
   });
 });
