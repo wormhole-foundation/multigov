323cf9342dc420f9bb15390941c6047b87bf34f5
marcomariscal <42938673+marcomariscal@users.noreply.github.com>
2024-10-30 10:18:10 -0700

    feat: biome at root to fix format on save issues

--- integration-tests/test/voteFromSpoke/helpers.ts
+++ integration-tests/test/voteFromSpoke/helpers.ts
@@ -1,3 +1,7 @@
 export const voteFromSpoke = async () => {
   // TODO: Add the rest of the logic to vote from spoke
 };
+
+export const bridgeVotesToHub = async () => {
+  // TODO: Add the rest of the logic to bridge votes to hub
+};
