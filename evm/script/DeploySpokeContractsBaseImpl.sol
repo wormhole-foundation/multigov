40ae475a5016467d844e2861d011da318d2db7d5
marcomariscal <marco.a.mariscal@gmail.com>
2024-10-31 14:37:28 -0700

    fix: func to non pure

--- evm/script/DeploySpokeContractsBaseImpl.sol
+++ evm/script/DeploySpokeContractsBaseImpl.sol
@@ -37,7 +37,7 @@ abstract contract DeploySpokeContractsBaseImpl is Script {
 
   error InvalidAddressConfiguration();
 
-  function _getDeploymentConfiguration() internal pure virtual returns (DeploymentConfiguration memory);
+  function _getDeploymentConfiguration() internal virtual returns (DeploymentConfiguration memory);
 
   function _deploymentWallet() internal virtual returns (Vm.Wallet memory) {
     uint256 deployerPrivateKey = vm.envOr("DEPLOYER_PRIVATE_KEY", DEFAULT_DEPLOYER_PRIVATE_KEY);
