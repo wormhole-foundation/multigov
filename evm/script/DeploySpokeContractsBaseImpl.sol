0ce1379948142ec5706b5637042a693b7da63687
marcomariscal <42938673+marcomariscal@users.noreply.github.com>
2024-11-08 12:20:36 -0800

    fix: revert

--- evm/script/DeploySpokeContractsBaseImpl.sol
+++ evm/script/DeploySpokeContractsBaseImpl.sol
@@ -37,13 +37,13 @@ abstract contract DeploySpokeContractsBaseImpl is Script {
 
   error InvalidAddressConfiguration();
 
-  function _getDeploymentConfiguration() internal virtual returns (DeploymentConfiguration memory);
+  function _getDeploymentConfiguration() internal pure virtual returns (DeploymentConfiguration memory);
 
   function _deploymentWallet() internal virtual returns (Vm.Wallet memory) {
     uint256 deployerPrivateKey = vm.envOr("DEPLOYER_PRIVATE_KEY", DEFAULT_DEPLOYER_PRIVATE_KEY);
 
-    Vm.Wallet memory wallet = vm.createWallet(deployerPrivateKey);
-    if (deployerPrivateKey == DEFAULT_DEPLOYER_PRIVATE_KEY) revert InvalidAddressConfiguration();
+    Vm.Wallet memory defaultWallet = vm.createWallet(DEFAULT_DEPLOYER_PRIVATE_KEY);
+    if (defaultWallet.addr == wallet.addr) revert InvalidAddressConfiguration();
     return wallet;
   }
 
