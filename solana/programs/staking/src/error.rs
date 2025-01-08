98fbba2e6c276e1c7e90987f52bf33e3c3b37a16
zpoken <82594516+zpoken@users.noreply.github.com>
2025-01-08 17:45:28 +0200

    Add check wormhole chain id (#222)

--- solana/programs/staking/src/error.rs
+++ solana/programs/staking/src/error.rs
@@ -159,4 +159,6 @@ pub enum MessageExecutorError {
     VaaNotFinalized,
     #[msg("Missing Remaining account")]
     MissedRemainingAccount,
+    #[msg("Message is not meant for this chain")]
+    InvalidWormholeChainId,
 }
