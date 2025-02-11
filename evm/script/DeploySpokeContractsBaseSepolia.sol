bd6d360be5199c0ec72f59b2f8ec8e821bf874c8
marcomariscal <42938673+marcomariscal@users.noreply.github.com>
2025-02-11 09:15:20 -0800

    feat: base sepolia deploy script

---
+++ evm/script/DeploySpokeContractsBaseSepolia.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: Apache 2
+pragma solidity ^0.8.23;
+
+import {DeploySpokeContractsBaseImpl} from "script/DeploySpokeContractsBaseImpl.sol";
+
+contract DeploySpokeContractsBaseSepolia is DeploySpokeContractsBaseImpl {
+  function _getDeploymentConfiguration() internal pure override returns (DeploymentConfiguration memory) {
+    return DeploymentConfiguration({
+      wormholeCore: 0x79A1027a6A159502049F10906D333EC57E95F083, // Base Sepolia Wormhole Core
+      hubChainId: 10_002, // Sepolia hub chain ID
+      hubProposalMetadata: 0xc77B1FDc68E9E0BaccD55696Ec00BDaE0271b241,
+      votingToken: 0x74f00907CFC6E44Fb72535cdD1eC52a37EacAbE4,
+      voteWeightWindow: 10 minutes,
+      hubDispatcher: 0x000000000000000000000000059e356b519ef25535ea6bf53edfdc07bfdd730e,
+      spokeChainId: 10_004 // Base Sepolia Wormhole chain ID
+    });
+  }
+}
