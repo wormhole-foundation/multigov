52a219080996e32008ceca8e5fcfc3c247669987
marcomariscal <42938673+marcomariscal@users.noreply.github.com>
2025-02-11 10:59:35 -0800

    feat: redeploy with base sepolia deployed voting token

--- evm/script/DeploySpokeContractsBaseSepolia.sol
+++ evm/script/DeploySpokeContractsBaseSepolia.sol
@@ -9,7 +9,7 @@ contract DeploySpokeContractsBaseSepolia is DeploySpokeContractsBaseImpl {
       wormholeCore: 0x79A1027a6A159502049F10906D333EC57E95F083, // Base Sepolia Wormhole Core
       hubChainId: 10_002, // Sepolia hub chain ID
       hubProposalMetadata: 0xc77B1FDc68E9E0BaccD55696Ec00BDaE0271b241,
-      votingToken: 0x74f00907CFC6E44Fb72535cdD1eC52a37EacAbE4,
+      votingToken: 0x12Fb7f85dea3A3F83018E4A647dC8B0456dF9B39,
       voteWeightWindow: 10 minutes,
       hubDispatcher: 0x000000000000000000000000059e356b519ef25535ea6bf53edfdc07bfdd730e,
       spokeChainId: 10_004 // Base Sepolia Wormhole chain ID
