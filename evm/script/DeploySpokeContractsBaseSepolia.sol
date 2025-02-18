7240f02cb818136043e98b42c936d53dbb329ccb
marcomariscal <42938673+marcomariscal@users.noreply.github.com>
2025-02-18 10:19:46 -0800

    fix: correct addrs

--- evm/script/DeploySpokeContractsBaseSepolia.sol
+++ evm/script/DeploySpokeContractsBaseSepolia.sol
@@ -8,10 +8,10 @@ contract DeploySpokeContractsBaseSepolia is DeploySpokeContractsBaseImpl {
     return DeploymentConfiguration({
       wormholeCore: 0x79A1027a6A159502049F10906D333EC57E95F083, // Base Sepolia Wormhole Core
       hubChainId: 10_002, // Sepolia hub chain ID
-      hubProposalMetadata: 0xc77B1FDc68E9E0BaccD55696Ec00BDaE0271b241,
+      hubProposalMetadata: 0x1A3E5624769C3Dc9106347A239523e4A08d85C38,
       votingToken: 0x12Fb7f85dea3A3F83018E4A647dC8B0456dF9B39,
       voteWeightWindow: 10 minutes,
-      hubDispatcher: 0x000000000000000000000000059e356b519ef25535ea6bf53edfdc07bfdd730e,
+      hubDispatcher: 0x000000000000000000000000027e445be20889f0e6ddd30372842e6c183d3648b,
       spokeChainId: 10_004 // Base Sepolia Wormhole chain ID
     });
   }
