681896492b3ac025363ab0cba8262760ff212fd0
zpoken <82594516+zpoken@users.noreply.github.com>
2025-03-06 18:48:12 +0200

    Add scripts for initialize staking program in mainnet

--- solana/app/deploy/mainnet/constants.ts
+++ solana/app/deploy/mainnet/constants.ts
@@ -3,11 +3,11 @@ import { homedir } from "os";
 import { loadKeypair } from "../../../tests/utils/keys";
 import { contracts } from "@wormhole-foundation/sdk-base";
 
-export const RPC_NODE = "https://api.devnet.solana.com";
+export const RPC_NODE = "https://api.mainnet-beta.solana.com";
 
-/// Wormhole Token (W) (devnet solana address)
+/// Wormhole Token (W) (mainnet solana address)
 export const WORMHOLE_TOKEN = new PublicKey(
-  "Exne2kdeGToBnC2WVSdt1gLy6fjnNftbPtsCPx8AuL7V",
+  "85VBFQZC9TZkfaptBWjvUw7YbZjy52A6mjtPGjstQAmQ",
 );
 
 export const STAKING_ADDRESS = new PublicKey(
@@ -15,12 +15,13 @@ export const STAKING_ADDRESS = new PublicKey(
 );
 
 export const CORE_BRIDGE_PID = new PublicKey(
-  contracts.coreBridge.get("Testnet", "Solana")!, // 3u8hJUVTA4jH1wYAyUur7FFZVQ8H635K3tSHHF4ssjQ5
+  contracts.coreBridge.get("Mainnet", "Solana")!, // worm2ZoG2kUd4vFXhvjh93UUH596ayRfgQ2MgjNMTth
 );
+console.log(CORE_BRIDGE_PID);
 
-/// Wormhole Hub Proposal Metadata Contract (sepolia ethereum address)
+/// Wormhole Hub Proposal Metadata Contract (mainnet ethereum address)
 export const HUB_PROPOSAL_METADATA_ADDRESS =
-  "0x1a3e5624769c3dc9106347a239523e4a08d85c38";
+  "0x1a3e5624769c3dc9106347a239523e4a08d85c38"; // TODO: needs to be updated after a deployment to ethereum mainnet
 export const hubProposalMetadataUint8Array = new Uint8Array(
   HUB_PROPOSAL_METADATA_ADDRESS.slice(2)
     .toLowerCase()
@@ -29,15 +30,14 @@ export const hubProposalMetadataUint8Array = new Uint8Array(
 );
 
 /// Wormhole Hub Chain ID
-// https://wormhole.com/docs/build/reference/chain-ids/#__tabbed_1_2
-export const HUB_CHAIN_ID = 10002; // SEPOLIA
+// https://wormhole.com/docs/build/reference/chain-ids/#__tabbed_1_1
+export const HUB_CHAIN_ID = 2; // Mainnet
 
 export const CHECKPOINTS_ACCOUNT_LIMIT = 654998;
-export const TEST_CHECKPOINTS_ACCOUNT_LIMIT = 15;
 
-/// Wormhole hubSolanaMessageDispatcher Contract (sepolia ethereum address)
+/// Wormhole hubSolanaMessageDispatcher Contract (mainnet ethereum address)
 export const HUB_SOLANA_MESSAGE_DISPATCHER_ADDRESS =
-  "0xaeb78fb7ddedbbcab908e91e94f1fb04a23fbce5";
+  "0xaeb78fb7ddedbbcab908e91e94f1fb04a23fbce5"; // TODO: needs to be updated after a deployment to ethereum mainnet
 const hubSolanaMessageDispatcherHex20 =
   HUB_SOLANA_MESSAGE_DISPATCHER_ADDRESS.slice(2);
 const hubSolanaMessageDispatcherHex32 =
@@ -54,18 +54,6 @@ export const hubSolanaMessageDispatcherPublicKey = new PublicKey(
 );
 // console.log("hubSolanaMessageDispatcherPublicKey:", hubSolanaMessageDispatcherPublicKey);
 
-/// Wormhole HubGovernor Contract (sepolia ethereum address)
-export const HUB_GOVERNOR_ADDRESS =
-  "0x491f237e770ab05da1e40658fab531f8a5acf7a8";
-
-/// Wormhole HubSolanaSpokeVoteDecoder Contract (sepolia ethereum address)
-export const HUB_SOLANA_SPOKE_VOTE_DECODER_ADDRESS =
-  "0x40e80d89d61d023568db528090c3e56ae472b0e5";
-
-/// Wormhole HubVotePool Contract (sepolia ethereum address)
-export const HUB_VOTE_POOL_ADDRESS =
-  "0x1004c781763c70f5f11aa64b0e5b34e1442a3c02";
-
 export const DEPLOYER_AUTHORITY_PATH = "/.config/solana/deployer.json";
 export const DEPLOYER_AUTHORITY_KEYPAIR = loadKeypair(
   homedir() + DEPLOYER_AUTHORITY_PATH,
@@ -80,20 +68,6 @@ export const GOVERNANCE_AUTHORITY_KEYPAIR = loadKeypair(
 export const VESTING_ADMIN_PATH = "/.config/solana/vestingAdmin.json";
 export const VESTING_ADMIN_KEYPAIR = loadKeypair(
   homedir() + VESTING_ADMIN_PATH,
-);
-
-export const USER_AUTHORITY_PATH = "/.config/solana/user.json";
-export const USER_AUTHORITY_KEYPAIR = loadKeypair(
-  homedir() + USER_AUTHORITY_PATH,
-);
-
-export const USER2_AUTHORITY_PATH = "/.config/solana/user2.json";
-export const USER2_AUTHORITY_KEYPAIR = loadKeypair(
-  homedir() + USER2_AUTHORITY_PATH,
-);
-
-export const AIRLOCK_PDA_ADDRESS = new PublicKey(
-  "2ejzW2eFPedskg1KcrjcFs9g1JorRVcMes1TBPpGbhdy",
 );
 
 export const VOTE_WEIGHT_WINDOW_LENGTHS = 10 * 60; // 10 minutes
