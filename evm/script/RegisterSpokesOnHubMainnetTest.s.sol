5d8b102605bba0f2e1bb26b466bd8c2a085ce618
keating <keating.dev@protonmail.com>
2025-03-14 14:20:54 -0400

    Add register

--- evm/script/RegisterSpokesOnHubMainnetTest.s.sol
+++ evm/script/RegisterSpokesOnHubMainnetTest.s.sol
@@ -5,12 +5,15 @@ import {Script, stdJson} from "forge-std/Script.sol";
 import {Vm} from "forge-std/Vm.sol";
 import {HubVotePool} from "src/HubVotePool.sol";
 
-contract RegisterSpokesOnHubTestnet is Script {
-  address HUB_VOTE_POOL = 0xddEB0415Ada159AE53D980feB6FF05244F65FD7f; // TODO: Replace with the hub vote pool address
-  address OPTIMISM_SEPOLIA_VOTE_AGGREGATOR = 0x767f74378aCAFDb44C7E2f73F49101aD2C3eD6d2; // TODO: Replace with a real
+contract RegisterSpokesOnHubMainnetTest is Script {
+  address HUB_VOTE_POOL = 0x6D87469dC04aec896dB03Df9B1b9Ba29535CC206; // TODO: Replace with the hub vote pool address
+  address OPTIMISM_VOTE_AGGREGATOR = 0x557133FB5451aB1CB83FA271FC044eEBea8faaE2; // TODO: Replace with a real
+  address BASE_VOTE_AGGREGATOR = 0xdcd97449bFD93c554e06C4B0212A9faF66d9Ea3e; // TODO: Replace with a real
+  address ARBITRUM_VOTE_AGGREGATOR = 0x87D07772C1150A0e62C1D9171AdaF4C1F17888EB; // TODO: Replace with a real
+
     // address
-  bytes32 SOLANA_SPOKE = bytes32(0xabd58849f17e52708082849880f862589c11f972cb372d73b0cd219722cd0f22);
-  address TIMELOCK = 0x1054f49899Af83e0c55375d54D2F57488cFC8606; // TODO Timelock address
+  bytes32 SOLANA_SPOKE = bytes32(0xee7066afc36b670f4b52088b82a96da0ba563335db5ac099786d9f8800ff429e);
+  address TIMELOCK = 0x0fAA8fc7A60809B3557d5Dbe463B64F94de5ac06; // TODO Timelock address
 
   error InvalidAddressConfiguration();
 
@@ -29,7 +32,9 @@ contract RegisterSpokesOnHubTestnet is Script {
     HubVotePool hubVotePool = HubVotePool(HUB_VOTE_POOL);
     // register solana
     hubVotePool.registerSpoke(1, SOLANA_SPOKE);
-    hubVotePool.registerSpoke(10_005, bytes32(uint256(uint160(OPTIMISM_SEPOLIA_VOTE_AGGREGATOR))));
+    hubVotePool.registerSpoke(24, bytes32(uint256(uint160(OPTIMISM_VOTE_AGGREGATOR))));
+    hubVotePool.registerSpoke(23, bytes32(uint256(uint160(ARBITRUM_VOTE_AGGREGATOR))));
+    hubVotePool.registerSpoke(30, bytes32(uint256(uint160(BASE_VOTE_AGGREGATOR))));
     // transfer owner
     //hubVotePool.transferOwnership(TIMELOCK);
     vm.stopBroadcast();
